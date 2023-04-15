8d417336a97072dac99718b68fc216d4e39b3e9e
Dheeraj-2205 <djsays124@gmail.com>
2023-04-15 21:58:19 +0530

    adding header and logo

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,10 +1,17 @@
 
-import './App.css';
-
+import './App.scss';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from './components/Home';
+import Header from './components/header/Header';
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path = "/" element = {<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ netflix/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../Images/logo.png";
+const Header = () => {
+  return (
+    <div className='header'>
+        <nav>
+            <img src={logo} alt="" />
+        </nav>
+    </div>
+  )
+}
+
+export default Header
