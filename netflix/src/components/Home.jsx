8458ea5165e584ec6ce3306dbd0314f12a29d48f
Dheeraj-2205 <djsays124@gmail.com>
2023-04-16 02:03:52 +0530

    adding popular netflix items

--- netflix/src/components/Home.jsx
+++ netflix/src/components/Home.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import "./Home.scss";
+
+const Card =   ({img}) =>(
+
+     <img className = "card" src={img} alt="cover" />
+)
+const Row = ({title}) =>{
+    return(
+
+        <div className="row">
+            <h2>{title}</h2>
+            <div>
+                
+                <Card img = {"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"}/>
+                <Card img = {"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"}/>
+                <Card img = {"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"}/>
+                <Card img = {"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"}/>
+                <Card img = {"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"}/>
+                <Card img = {"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"}/>
+
+            </div>
+        </div>
+    )
+}
 const Home = () => {
   return (
     <section className='home'>
-        <div className='banner'>
+        <div className='banner'></div>
 
-
-        </div>
-        <Raw/>
+        <Row title = {"Popular On Netflix"}/>
     </section>
   )
 }
