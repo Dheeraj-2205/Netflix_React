c0ea1d2b1708d0b2c45a441578a321c05f28f7b8
Dheeraj-2205 <djsays124@gmail.com>
2023-04-16 02:16:52 +0530

    working on array to map popular movies

--- netflix/src/components/Home.jsx
+++ netflix/src/components/Home.jsx
@@ -5,15 +5,17 @@ const Card =   ({img}) =>(
 
      <img className = "card" src={img} alt="cover" />
 )
-const Row = ({title,arr = []}) =>{
+const Row = ({title,arr = [{
+    img :"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"
+}] }) =>{
     return(
 
         <div className="row">
             <h2>{title}</h2>
             <div>
                 {
-                    arr.map(()=>(
-                        <Card img = {"https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg"}/>
+                    arr.map((ele)=>(
+                        <Card img = {ele.img}/>
                     ))
                 }
             </div>
