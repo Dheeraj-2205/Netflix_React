7a4f49aa7f832876cded22294b7ff710505cb7ff
Dheeraj-2205 <djsays124@gmail.com>
2023-04-16 15:25:00 +0530

    adding genre

--- netflix/src/components/Home.jsx
+++ netflix/src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import "./Home.scss";
 import axios from "axios";
-
+import {Link} from "react-router-dom"
 
 
 const apikey = "fc399f902305262547b5477e6a3138e2";
 const url = "https://api.themoviedb.org/3"
 const upcoming = "upcoming";
+const nowPlaying = "now_playing";
+const Popular = "popular";
+const top_rated = "top_rated";
 const imgUrl = "https://image.tmdb.org/t/p/original" 
 const Card =   ({img}) =>(
 
@@ -29,23 +32,53 @@ const Row = ({ title,arr = [] }) =>{
 }
 const Home = () => {
     const [upcomingMovie,setUpcomingMovie] = useState([]);
+    const [play,setPlay] = useState([]);
+    const [popular,setPopular] = useState([]);
+    const [rated,setRated] = useState([]);
+    const [genre,setGenre] = useState([]);
     useEffect(()=>{
         const fetchData = async()=>{
             const {data : {results}} = await axios.get(`${url}/movie/${upcoming}?api_key=${apikey}`);
-            // console.log(results);
             setUpcomingMovie(results);
-            console.log(upcomingMovie);
+        }
+        const fetchnowPlaying = async()=>{
+            const {data : {results}} = await axios.get(`${url}/movie/${nowPlaying}?api_key=${apikey}`);
+            setPlay(results);
+        }
+        const fetchPopular = async()=>{
+            const {data : {results}} = await axios.get(`${url}/movie/${Popular}?api_key=${apikey}`);
+            setPopular(results);
+        }
+        const fetchtop_rated = async()=>{
+            const {data : {results}} = await axios.get(`${url}/movie/${top_rated}?api_key=${apikey}`);
+            setRated(results);
+        }
+        const getAllGenre = async()=>{
+            const {data :{genres}} = await axios.get(`${url}/genre/movie/list?api_key=${apikey}`);
+            setGenre(genres);
         }
         fetchData();
+        fetchnowPlaying();
+        fetchPopular();
+        fetchtop_rated();
+        getAllGenre();
     },[])
   return (
     <section className='home'>
         <div className='banner'></div>
 
         <Row title = {"Popular On Netflix"} arr = {upcomingMovie}/>
-        <Row title = {"Popular On Netflix"}/>
-        <Row title = {"Popular On Netflix"}/>
-        <Row title = {"Popular On Netflix"}/>
+        <Row title = {"Popular On Netflix"} arr = {play}/>
+        <Row title = {"Popular"} arr = {popular}/>
+        <Row title = {"Top Rated"} arr = {rated}/>
+
+        <div className="genreBox">
+            {
+                genre.map((item, i )=>(
+                    <Link key = {i} to= {`/genre/${item.id}`}>{item.name}</Link>
+                ))
+            }
+        </div>
     </section>
   )
 }
